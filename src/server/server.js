32bbe3312e95fa2ccefa91b658b0335e2276ee00
stargaz <khadafischool@gmail.com>
2024-05-19 19:00:20 +0700

    insert error handling

--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 const loadModel = require('../services/loadModel');
+const InputError = require('../exceptions/InputError');
 
 (async () => {
   const server = Hapi.server({
@@ -24,19 +25,19 @@ const loadModel = require('../services/loadModel');
 server.ext('onPreResponse', function (request, h) {
     const response = request.response;
 
-    // if (response instanceof InputError) {
-    //     const newResponse = h.response({
-    //         status: 'fail',
-    //         message: `${response.message} Terjadi kesalahan dalam melakukan prediksi`,
-    //     })
-    //     newResponse.code(response.output.statusCode)
-    //     return newResponse;
-    // }
+    if (response instanceof InputError) {
+        const newResponse = h.response({
+            status: 'fail',
+            message: `${response.message}`
+        })
+        newResponse.code(response.statusCode)
+        return newResponse;
+    }
     
     if (response.isBoom) {
       const newResponse = h.response({
         status: 'fail',
-        message: response.message,
+        message: response.message
       })
       newResponse.code(response.output.statusCode)
       return newResponse;
--- src/services/inference.js
+++ src/services/inference.js
@@ -1,26 +1,31 @@
 const tf = require('@tensorflow/tfjs-node');
+const InputError = require('../exceptions/InputError');
 
 async function predictClassificationModel(model, image){
+  try {
     const tensor = tf.node 
-    .decodeImage(image)
-    .resizeNearestNeighbor([224,224])
-    .expandDims()
-    .toFloat()
+      .decodeImage(image)
+      .resizeNearestNeighbor([224,224])
+      .expandDims()
+      .toFloat()
 
-    const prediction = model.predict(tensor);
-    const score = await prediction.data();
-    const confidenceScore = score[0] * 100; // Hanya satu nilai karena binary classification
+      const prediction = model.predict(tensor);
+      const score = await prediction.data();
+      const confidenceScore = score[0] * 100; 
 
-  const label = confidenceScore > 50 ? 'Cancer' : 'Non-cancer';
+    const label = confidenceScore > 50 ? 'Cancer' : 'Non-cancer';
 
-  let suggestion;
-  if (label === 'Cancer') {
-    suggestion = "Segera periksa ke dokter!";
-  } else {
-    suggestion = "Anda sehat!";
+    let suggestion;
+    if (label === 'Cancer') {
+      suggestion = "Segera periksa ke dokter!";
+    } else {
+      suggestion = "Anda sehat!";
+    }
+
+    return { label, confidenceScore, suggestion };
+  } catch (error) {
+    throw new InputError('Terjadi kesalahan dalam melakukan prediksi');
   }
-
-  return { label, confidenceScore, suggestion };
 }
 
 module.exports = predictClassificationModel;
